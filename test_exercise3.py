b3bf13247e02ccb9917b1f7ca0870ed685046faa
misquit3 <tania.misquitta@mail.utoronto.ca>
2014-10-05 20:40:20 -0400

    add test case scenarios

--- test_exercise3.py
+++ test_exercise3.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
+<<<<<<< HEAD
+import pytest
+
+from exercise3 import decide_rps
+
+def test_checksum():
+    """
+    Inputs that are the correct format and length
+
+    """
+       # check that expected input behaves correctly
+    assert decide_rps("Rock", "Rock") == 0
+    assert decide_rps("Rock", "Paper") == 2
+    assert decide_rps("Rock", "Scissors") == 1
+    assert decide_rps("Scissors", "Scissors") == 0
+    assert decide_rps("Scissors", "Paper") == 1
+    assert decide_rps("Scissors", "Rock") == 2
+    assert decide_rps("Paper", "Paper") == 0
+    assert decide_rps("Paper", "Scissors") == 2
+    assert decide_rps("Paper", "Rock") == 1
+    # check that correct exception is thrown for unexpected value
+    with pytest.raises(ValueError):
+        decide_rps("Rock", "Banana") == 1
+
+    # check that correct exception is thrown for bad type
+    with pytest.raises(TypeError):
+        decide_rps("Rock", 2) == 1
+=======
 
 import pytest
 from exercise3 import decide_rps
@@ -11,5 +40,8 @@ def test_checksum():
     assert decide_rps("Rock", "Paper") == 2
     assert decide_rps("Scissors", "Scissors") == 0
     assert decide_rps("Rock", "Scissors") == 1
+>>>>>>> origin/master
     # other tests
+
+
 
